4d3fb252298d71f16b71ad8a50c6e041fe0b324d
Gabriel Toledo <gabrielreb7@gmail.com>
2021-10-01 14:54:50 -0300

    inicio de projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { MaterialModule } from './material/material.module';
+import { MenuComponent } from './views/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD123BaZuUWlHGFC1MsmNGYhYD-bM3kLn0",
+    authDomain: "angularcrud-ca173.firebaseapp.com",
+    databaseURL: "https://angularcrud-ca173-default-rtdb.firebaseio.com",
+    projectId: "angularcrud-ca173",
+    storageBucket: "angularcrud-ca173.appspot.com",
+    messagingSenderId: "40554446569",
+    appId: "1:40554446569:web:9f14837eaf35f65a367a78"
+    }
 };
